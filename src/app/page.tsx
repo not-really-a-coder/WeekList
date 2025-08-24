390ce6ca2ed29cc941654f033bc75ab6c33ef973
Leonid Ardaev <leonid.ardaev@gmail.com>
2025-08-24 09:57:01 +0000

    Make the horizontal padding of the taskgrid in the desktop version the s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -456,7 +456,7 @@ export default function Home() {
         <Header />
         <main className="flex-grow py-4">
           <div className="mx-auto px-0 sm:px-2">
-            <div className="flex items-center justify-between mb-4 md:mb-8 px-4">
+            <div className="flex items-center justify-between mb-4 md:mb-8 px-4 sm:px-2">
               <Button variant="outline" size="icon" onClick={goToPreviousWeek} aria-label="Previous week">
                   <ChevronLeft className="h-4 w-4" />
               </Button>
