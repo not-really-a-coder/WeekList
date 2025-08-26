b25c2f77b6c0264737dff1ddfb2634396a41e7f8
Leonid Ardaev <leonid.ardaev@gmail.com>
2025-08-26 08:16:29 +0000

    In the hamburger menu, instead of the "Toggle theme" label, show the nam

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
 import {
   DropdownMenuItem,
   DropdownMenuPortal,
@@ -14,14 +13,16 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
+
+  const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 
   return (
     <DropdownMenuSub>
         <DropdownMenuSubTrigger>
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="ml-2">Toggle theme</span>
+            <span className="ml-2">Theme: {theme ? capitalize(theme) : 'System'}</span>
         </DropdownMenuSubTrigger>
         <DropdownMenuPortal>
             <DropdownMenuSubContent>
